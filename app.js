2849e006f85451a22b7b5faebe78a76b50312576
史开乐 <csurfssecure@gmail.com>
2022-11-06 09:42:16 -0500

    inquirer module

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+
+// const [name, github] = profileDataArgs;
 
 
-fs.writeFile('./index.html', generatePage(name, github), err =>{
-    if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err =>{
+//     if (err) throw new Error(err);
 
-    console.log('Portfolio complete! Check out index.html to see output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see output!');
+// });
+
