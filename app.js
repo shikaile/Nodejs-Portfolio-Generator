72347ae52812c7bc0a73658dcfc07bb81f91e6ea
史开乐 <csurfssecure@gmail.com>
2022-11-06 19:04:50 -0500

    validate

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name?',
+            validate: nameInput => {
+                if(nameInput){
+                    return true;
+                } else {
+                    console.log('Enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username',
+            validate: gitName => {
+                if(gitName){
+                    return true;
+                } else {
+                    console.log('Enter your Gitname!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -36,12 +52,28 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: projectName => {
+                if(projectName){
+                    return true;
+                } else {
+                    console.log('Enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: projectDescription => {
+                if(projectDescription){
+                    return true;
+                } else {
+                    console.log('Enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -52,7 +84,15 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: gitLink => {
+                if(gitLink){
+                    return true;
+                } else {
+                    console.log('Enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
