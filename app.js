52dba15ba7163332be3778b15381a53223763dbb
史开乐 <csurfssecure@gmail.com>
2022-11-09 07:39:06 -0800

    working features

--- app.js
+++ app.js
@@ -1,5 +1,54 @@
-
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template.js');
+
+//for temp use to be deleted
+const mockData = {
+    name: 'Lernantino',
+    github: 'lernantino',
+    confirmAbout: true,
+    about:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+    
+        projects: [
+        {
+        name: 'Run Buddy',
+        description:
+            'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['HTML', 'CSS'],
+        link: 'https://github.com/lernantino/run-buddy',
+        feature: true,
+        confirmAddProject: true
+        },
+        {
+        name: 'Taskinator',
+        description:
+            'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['JavaScript', 'HTML', 'CSS'],
+        link: 'https://github.com/lernantino/taskinator',
+        feature: true,
+        confirmAddProject: true
+        },
+        {
+        name: 'Taskmaster Pro',
+        description:
+            'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+        link: 'https://github.com/lernantino/taskmaster-pro',
+        feature: false,
+        confirmAddProject: true
+        },
+        {
+        name: 'Robot Gladiators',
+        description:
+            'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+        languages: ['JavaScript'],
+        link: 'https://github.com/lernantino/robot-gladiators',
+        feature: false,
+        confirmAddProject: false
+        }
+    ]
+};
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -131,22 +180,27 @@ const promptProject = portfolioData => {
     });
 };
 
+// promptUser()
+// .then(promptProject)
+// .then(portfolioData =>{
+//     const pageHTML = generatePage(portfolioData);
+//     // fs.writeFile('./index.html', pageHTML, err =>{
+//     //     if (err) throw new Error(err);
+    
+//     //     console.log('Page created! Check out index.html to see output!');
+//     // });
+// });
 
-promptUser()
-.then(promptProject)
-.then(portfolioData =>{
-    console.log(portfolioData);
-});
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
+//for temp use, to be deleted
+const pageHTML = generatePage(mockData);
+fs.writeFile('./index.html', pageHTML, err =>{
+            if (err) throw new Error(err);
+        
+            console.log('Page created! Check out index.html to see output!');
+        });
 
 // const [name, github] = profileDataArgs;
 
 
-// fs.writeFile('./index.html', generatePage(name, github), err =>{
-//     if (err) throw new Error(err);
 
-//     console.log('Portfolio complete! Check out index.html to see output!');
-// });
 
